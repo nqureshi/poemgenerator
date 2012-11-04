dd1d6e7567bfe8519efc51a626ed81f95ea14cf2
Nabeel Qureshi <nabeelqureshi67@gmail.com>
2012-11-04 14:33:08 +0000

    refactor and speed improvements

--- poemgenerator.rb
+++ poemgenerator.rb
@@ -2,27 +2,21 @@ require 'open-uri'
 require 'nokogiri'
 require 'sinatra'
 
+URL = "http://rpo.library.utoronto.ca/poems"
+
 helpers do
-  def get_poem_title(url) # Gets the title of a given poem using the CSS selector
-    poem_title = Nokogiri::HTML(open(url)).css("#page-title")
-    poem_title
-  end
-
-  def get_poem_author(url) # Gets the author of a given poem using the CSS selector
-    poem_author = Nokogiri::HTML(open(url)).css(".poet-name-in-poem")
-    poem_author
-  end
-
-  def get_poem_text(url) # Gets the text of the poem, to be iterated over using a block in the view
-    poem_text = Nokogiri::HTML(open(url)).css(".line-text")
-    poem_text
-  end
-
   def get_random_poem # Gets a random poem from the RPO library of poems
-    url = "http://rpo.library.utoronto.ca/poems"
-    poem_url = "http://rpo.library.utoronto.ca" + Nokogiri::HTML(open(url)).css("#block-system-main a")[rand(1..4785)]["href"]
-    poem_url
+    poem_url = Nokogiri::HTML(open(URL)).css("#block-system-main a")[rand(1..4785)]["href"]
+    URL.gsub("/poems", "") + poem_url
   end
+end
+
+before do
+  pass if request.path_info == "/"
+  @poem = Nokogiri::HTML(open(get_random_poem))
+  @poem_title = @poem.css("#page-title")
+  @poem_author = @poem.css(".poet-name-in-poem")
+  @poem_text = @poem.css(".line-text")
 end
 
 get '/' do
@@ -30,9 +24,5 @@ get '/' do
 end
 
 get '/poem' do
-  @poem_url = get_random_poem
-  @poem_title = get_poem_title(@poem_url)
-  @poem_author = get_poem_author(@poem_url)
-  @poem_text = get_poem_text(@poem_url)
   erb :poem
 end
